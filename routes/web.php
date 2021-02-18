20ba7bab6139381a5ed4a5bab85fde238f6ba647
shadowaion <chiluka_sasha@mail.ru>
2021-02-18 16:58:57 +0300

    Added 'customStyle.css' file. Modified view 'main'. Added 'urls' and 'urls/id' routes.

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('main');
+});
+
+Route::get('/urls', function () {
     return view('welcome');
 });
 
+Route::get('/urls/{id}', function () {
+    return view('welcome');
+});
