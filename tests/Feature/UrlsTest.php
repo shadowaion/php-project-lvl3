8fde157122ff5cb32d9763d6985cf0c2ec60970a
shadowaion <chiluka_sasha@mail.ru>
2021-02-26 19:43:37 +0300

    Changed test to work 'properly'.

--- tests/Feature/UrlsTest.php
+++ tests/Feature/UrlsTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use App\Models\Urls;
+use Carbon\Carbon;
 use Tests\TestCase;
 
 class UrlsTest extends TestCase
@@ -24,7 +25,6 @@ class UrlsTest extends TestCase
 
     //     $response->assertStatus(200);
     // }
-
     public function testIndex()
     {
         $response = $this->get(route('urls.index'));
@@ -35,26 +35,14 @@ class UrlsTest extends TestCase
     public function testStore()
     {
         $url = route('urls.index');
-        $name1 = 'https://example.org/blog/posts/how-to-test-code';
+        $name1 = 'https://example.com/blog/posts/how-to-test-code';
         $response = $this->post(route('urls.store'), ['url' => ['name' => $name1]]);
         $response->assertRedirect(route('urls.index'));
     }
 
     public function testShow()
     {
-        $id = 0;
-        $urls = DB::table('urls')
-                ->select(DB::raw('*'))
-                ->where('name', '=', 'https://example.org')
-                ->orderby('name')
-                ->get();
-        
-        if (count($urls) === 0) {
-            $urlData = Urls::factory()->create(['name' => 'https://example.org']);
-            $id = $urlData->id;
-        } else {
-            $id = $urls[0]->id;
-        }
+        $id = 1;
 
         $response = $this->call('GET', route('urls.show', ['id' => $id]));
 
@@ -62,30 +50,12 @@ class UrlsTest extends TestCase
     }
     public function testCheck()
     {
-        $urlsName1 = '';
-        $id1 = 0;
-
-        $urls = DB::table('urls')
-                ->select(DB::raw('*'))
-                ->where('name', '=', 'https://example.org')
-                ->orderby('name')
-                ->get();
-        
-        if (count($urls) === 0) {
-            $urlsData = Urls::factory()->create(['name' => 'https://example.org']);
-            $urlsName1 = $urlsData->name;
-            $id1 = $urlsData->id;
-        } else {
-            $urlsName1 = $urls[0]->name;
-            $id1 = $urls[0]->id;
-        }
+        $id = 1;
 
         Http::fake();
 
-        $response1 = $this->post(route('urls.check', ['id' => $id1]));
-        $responseStatus1 = Http::get($urlsName1)->getStatusCode();
+        $response1 = $this->post(route('urls.check', ['id' => $id]));
 
-        $this->assertEquals(200, $responseStatus1);
-        $response1->assertRedirect(route('urls.show', ['id' => $id1]));
+        $response1->assertRedirect(route('urls.show', ['id' => $id]));
     }
 }
