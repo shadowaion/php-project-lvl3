f1cb9aa6f8d8d1f58a61c776d4bf806b5eab8ed3
shadowaion <chiluka_sasha@mail.ru>
2021-02-27 01:05:23 +0300

    Attempt to use github actions database #11

--- client.js
+++ client.js
@@ -10,8 +10,8 @@ const pgclient = new Client({
 
 pgclient.connect();
 
-const table1 = 'CREATE TABLE urls(id bigint PRIMARY KEY GENERATE ALWAYS AS IDENTITY, name UNIQUE, created_at timestamp, updated_at timestamp)'
-const table2 = 'CREATE TABLE urls(id bigint PRIMARY KEY GENERATE ALWAYS AS IDENTITY, url_id bigint REFERENCES urls (id), status_code int, h1 varchar(255), keywords varchar(255), description varchar(255), created_at timestamp, updated_at timestamp)'
+const table1 = 'CREATE TABLE urls(id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name UNIQUE, created_at timestamp, updated_at timestamp)'
+const table2 = 'CREATE TABLE urls(id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY, url_id bigint REFERENCES urls (id), status_code int, h1 varchar(255), keywords varchar(255), description varchar(255), created_at timestamp, updated_at timestamp)'
 const text = 'INSERT INTO urls(id, name, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING *'
 const values = [99, 'http://joyreactor.cc', '2021-02-26 02:00:00', '2021-02-26 02:00:00']
 
