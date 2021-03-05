3eed214144cace39619e725351e8bb1f012f473a
shadowaion <chiluka_sasha@mail.ru>
2021-03-05 03:33:43 +0300

    Fixes phpstan errors #81

--- app/Http/Controllers/UrlCheckController.php
+++ app/Http/Controllers/UrlCheckController.php
@@ -98,7 +98,7 @@ class UrlCheckController extends Controller
         //echo "\n------------------Check 6-------------------\n";
 
         if (count($h1) > 0) {
-            $h1Text = (string) strip_tags($h1[0]);
+            $h1Text = strip_tags((string) $h1[0]);
         }
         if (count($keywords) > 0) {
             $keywordsContent = $keywords[0]->getAttribute('content');
